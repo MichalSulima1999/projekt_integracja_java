1ac41747f8041c6318348a2f14d2b24b45ac14fb
ArturSulej <sulejartur2@gmail.com>
2022-05-31 17:07:05 +0200

    Prototyp eksportu i importu XML, prototyp eksportu i importu JSON

--- src/main/java/com/example/projekt/file/JsonMarshaller.java
+++ src/main/java/com/example/projekt/file/JsonMarshaller.java
@@ -1,15 +1,20 @@
 package com.example.projekt.file;
 
 import com.example.projekt.domain.CpuEntity;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.*;
 import java.util.List;
 
 public class JsonMarshaller  {
 
     private final String url = "jdbc:postgresql://localhost/dvdrental";
-    private final String user = "postgres";
-    private final String password = "postgres";
+    private final String user = "root";
+    private final String password = "";
 
     public void insert(List<CpuEntity> list){
         String SQL = "INSERT INTO cpu(cores,threads,name,launch_date,lithography,base_frequency,turbo_frequency,tdp,socket,manufacturer)"
@@ -48,6 +53,48 @@ public class JsonMarshaller  {
     public Connection connect() throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
+    /*
+    public void export(String query) throws SQLException {
+        try(Connection conn = connect();
+            PreparedStatement pstmt = conn.prepareStatement(query))
+        {
+            ResultSet res = pstmt.executeQuery(query);
+            JsonArray jsonArray = convertToJSON(res);
+            FileWriter fileWriter = new FileWriter("json_file.json");
+            JsonObject obj = null;
+            fileWriter.write("[");
+            for(int i=0; i < jsonArray.length(); i++){
+                obj = (JsonObject) jsonArray.get(i);
+                if (i != jsonArray.length() - 1) {
+                    fileWriter.write(obj.toString() + ",\n");
+                } else {
+                    fileWriter.write(obj.toString());
+                }
+            }
+            fileWriter.write("]");
+            fileWriter.flush();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public JsonArray convertToJSON(ResultSet resultSet) throws SQLException {
+        JsonArray jsonArray = new JsonArray();
+        while(resultSet.next()){
+            JsonObject obj = null;
+            obj = new JsonObject();
+            int total_rows = resultSet.getMetaData().getColumnCount();
+            for(int i=0; i< total_rows; i++){
+                obj.put(resultSet.getMetaData().getColumnLabel(i+1));
+            }
+            jsonArray.put(obj);
+        }
+        return jsonArray;
+    }
+
+     */
 
 }
 
--- src/main/java/com/example/projekt/file/XmlMarshaller.java
+++ src/main/java/com/example/projekt/file/XmlMarshaller.java
@@ -1,24 +1,22 @@
 package com.example.projekt.file;
 
-import com.example.projekt.api.CpuResource;
 import com.example.projekt.domain.CpuEntity;
-import com.example.projekt.service.CpuEntityService;
-import com.example.projekt.service.CpuEntityServiceImplementation;
-import com.example.projekt.soap.cpu.CpuEndpoint;
-import com.example.projekt.soap.cpu.pl.integracja.soap.AddCpuRequest;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
 public class XmlMarshaller {
     static Procesory procesory = new Procesory();
 
     // Zapis do XML
-    public void marshall(String fileName) throws JAXBException, SQLException {
+    public void marshall(String filePath) throws JAXBException, SQLException {
         JAXBContext ctx = JAXBContext.newInstance(Procesory.class);
         Marshaller marshaller = ctx.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
@@ -27,24 +25,28 @@ public class XmlMarshaller {
         // Wyciągnięcie z bazy
         ResultSet rs = stat.executeQuery("select * from cpu");
         while(rs.next()){
-            CpuEntity p1 = new CpuEntity(rs.getInt(0),rs.getInt(1),rs.getString(2),rs.getDate(3),rs.getInt(4),rs.getInt(5),rs.getString(6),rs.getInt(7),rs.getString(8),rs.getString(9));
+            CpuEntity p1 = new CpuEntity(rs.getInt(0),rs.getInt(1),rs.getString(2),
+                    rs.getDate(3),rs.getInt(4),rs.getInt(5),rs.getString(6),
+                    rs.getInt(7),rs.getString(8),rs.getString(9));
             procesory.getProcesory().add(p1);
         }
 
-        marshaller.marshal(procesory, new File(fileName));
+        marshaller.marshal(procesory, new File(filePath));
     }
 
     // Odczyt z XML
-    public void unmarshall(String fileName) throws JAXBException {
+    public void unmarshall(String filePath) throws JAXBException {
         JAXBContext ctx = JAXBContext.newInstance(Procesory.class);
         Unmarshaller umarshaller = ctx.createUnmarshaller();
 
-        Procesory procesory = (Procesory) umarshaller.unmarshal(new File(fileName));
-
+        Procesory procesory = (Procesory) umarshaller.unmarshal(new File(filePath));
+        List<CpuEntity> dane = null;
         for(CpuEntity proc: procesory.getProcesory()){
             System.out.println(proc.getName());
+            dane.add(proc);
             // DODANIE DO BAZY
         }
+        procesory.setProcesory(dane);
 
     }
 }
